b0200d07d3ea1cbba2c2abd1a02c81ca73e6881c
Quora <quora@yolo-swag.com>
2015-05-14 04:04:11 +0200

    Bind to 0.0.0.0
    Thanks @dom96 !

--- web/web.nim
+++ web/web.nim
@@ -2,6 +2,10 @@ import jester, asyncdispatch, htmlgen, parsetoml, db_mysql
 
 include "./templates/main.tmpl"
 
+settings:
+  port = 5000.Port
+  bindAddr = "0.0.0.0"
+
 routes:
   get "/@channel?/?@date?":
     resp baseTemplate(h1("Hello, world!"))
