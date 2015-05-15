92e53d812b2215c9a8c7962eff2bba49a1b4b524
Quora <quora@yolo-swag.com>
2015-05-15 04:31:17 +0200

    use a macro for routing static content
    thanks @def-

--- web/web.nim
+++ web/web.nim
@@ -1,4 +1,4 @@
-import jester, asyncdispatch, htmlgen, parsetoml, db_mysql, times, strutils
+import jester, asyncdispatch, htmlgen, parsetoml, db_mysql, times, strutils, macros
 
 var t = parsetoml.parseFile("../sobot.toml")
 
@@ -16,43 +16,35 @@ var
   query = sql"SELECT id, mytime, nick, line FROM Chatlines WHERE target = ?"
 
 const
-  materializeCSS = staticRead "public/css/materialize.min.css"
-  materializeJS  = staticRead "public/js/materialize.min.js"
-  jqueryJS       = staticRead "public/js/jquery-2.1.1.min.js"
-  robotoregwoff2 = staticRead "public/font/roboto/Roboto-Regular.woff2"
-  robotoregwoff  = staticRead "public/font/roboto/Roboto-Regular.woff"
-  robotoregttf   = staticRead "public/font/roboto/Roboto-Regular.ttf"
   robotstxt      = staticRead "robots.txt"
 
 settings:
   port = 5000.Port
   bindAddr = "0.0.0.0"
 
-routes:
-  get "/static/css/materialize.min.css":
-    headers["Cache-Control"] = "public, max-age=31536000"
-    resp materializeCSS, "text/css"
-
-  get "/static/js/materialize.min.js":
-    headers["Cache-Control"] = "public, max-age=31536000"
-    resp materializeJS, "text/javascript"
-
-  get "/static/js/jquery-2.1.1.min.js":
-    headers["Cache-Control"] = "public, max-age=31536000"
-    resp jqueryJS, "text/javascript"
-
-  get "/static/font/roboto/Roboto-Regular.woff2":
-    headers["Cache-Control"] = "public, max-age=31536000"
-    resp robotoregwoff2, "application/font-woff2"
-
-  get "/static/font/roboto/Roboto-Regular.woff":
-    headers["Cache-Control"] = "public, max-age=31536000"
-    resp robotoregwoff, "application/font-woff"
-
-  get "/static/font/roboto/Roboto-Regular.ttf":
-    headers["Cache-Control"] = "public, max-age=31536000"
-    resp robotoregttf, "application/octet-stream"
-
+macro myRoutes(xs: expr, s: stmt): stmt {.immediate.} =
+  var list = newStmtList()
+  for x in xs.children:
+    let file = $x[0]
+    let mime = $x[1]
+    list.add parseExpr """
+      get "/static/$1":
+        const content = staticRead "public/$1"
+        headers["Cache-Control"] = "public, max-age=31536000"
+        resp content, "$2"
+    """ % [file, mime]
+  for x in s.children:
+    list.add x
+  result = newCall("routes", list)
+ 
+myRoutes([
+  ("css/materialize.min.css", "text/css"),
+  ("js/materialize.min.js", "text/javascript"),
+  ("js/jquery-2.1.1.min.js", "text/javascript"),
+  ("font/roboto/Roboto-Regular.woff2", "application/font-woff2"),
+  ("font/roboto/Roboto-Regular.woff", "application/font-woff"),
+  ("font/roboto/Roboto-Regular.ttf", "application/octet-stream"),
+]):
   get "/robots.txt":
     resp robotstxt, "text/plain"
 
