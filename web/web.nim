f1122e2f784c5a38ca3349a7785a907d8de69c11
Quora <quora@yolo-swag.com>
2015-05-17 02:19:56 +0200

    Make many improvements all over

--- web/web.nim
+++ web/web.nim
@@ -22,43 +22,18 @@ settings:
   port = 5000.Port
   bindAddr = "0.0.0.0"
 
-macro myRoutes(xs: expr, s: stmt): stmt {.immediate.} =
-  var list = newStmtList()
-  for x in xs.children:
-    let file = $x[0]
-    let mime = $x[1]
-    list.add parseExpr """
-      get "/static/$1":
-        const content = staticRead "public/$1"
-        headers["Cache-Control"] = "public, max-age=31536000"
-        resp content, "$2"
-    """ % [file, mime]
-  for x in s.children:
-    list.add x
-  result = newCall("routes", list)
- 
-myRoutes([
-  ("css/materialize.min.css", "text/css"),
-  ("js/materialize.min.js", "text/javascript"),
-  ("js/jquery-2.1.1.min.js", "text/javascript"),
-  ("font/roboto/Roboto-Regular.woff2", "application/font-woff2"),
-  ("font/roboto/Roboto-Regular.woff", "application/font-woff"),
-  ("font/roboto/Roboto-Regular.ttf", "application/octet-stream"),
-]):
+routes:
   get "/robots.txt":
     resp robotstxt, "text/plain"
 
   get "/":
     resp "No logs selected".errorPage.baseTemplate
 
-  get "/logs/?@channel?":
+  get "/logs/@channel":
     var
       channel = @"channel"
 
-    if channel == "":
-      channel = t.getString("bot.channel")
-    else:
-      channel = "#" & channel
+    channel = "#" & channel
 
     try:
       var rows = db.getAllRows(query, channel)
