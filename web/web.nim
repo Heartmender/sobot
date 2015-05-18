3c3ec7455f0af8fd51e4018b9723a3e3812bd7cf
Quora <quora@yolo-swag.com>
2015-05-18 03:47:02 +0200

    web: don't double-parse time

--- web/web.nim
+++ web/web.nim
@@ -14,9 +14,9 @@ var
   dbname = t.getString("database.database")
 
   db = db_mysql.open(dbhost, dbuser, dbpass, dbname)
-  query = sql"SELECT id, UNIX_TIMESTAMP(mytime), nick, line FROM Chatlines WHERE target = ?"
+  query = sql"SELECT id, mytime, nick, line FROM Chatlines WHERE target = ?"
   distinctquery = sql"SELECT DISTINCT target FROM Chatlines ORDER BY target;"
-  calloutquery = sql"SELECT id, target, UNIX_TIMESTAMP(mytime), nick, line FROM Chatlines WHERE target = ? ORDER BY id DESC LIMIT 1;"
+  calloutquery = sql"SELECT id, target, mytime, nick, line FROM Chatlines WHERE target = ? ORDER BY id DESC LIMIT 1;"
 
 const
   robotstxt      = staticRead "robots.txt"
